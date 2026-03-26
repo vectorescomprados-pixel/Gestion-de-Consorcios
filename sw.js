aa3d0d1d2cf6ca9ca2abedacdded1ff13dfcb9ae
vectorescomprados-pixel <vectorescomprados@gmail.com>
2026-03-26 15:44:35 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = 'hub-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './icono.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((k) => caches.delete(k)));
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
 });
